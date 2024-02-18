24a799443e3b2df62a5925e567ff4453617f4951
Acux-dev <juansup123@gmail.com>
2024-02-18 17:06:12 -0500

    ajustes pausa
    no recuerdo

--- GUI/pause_menu.gd
+++ GUI/pause_menu.gd
@@ -5,13 +5,19 @@ extends ColorRect
 @onready var settings_button : Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Settings_Button
 @onready var restart_button : Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Restart_Button
 @onready var exit_button : Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/Exit_Button
+@onready var canvas_settings = %CanvasSettings
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	resume_button.pressed.connect(unpause)
-	#settings_button.pressed.connect()
+	settings_button.pressed.connect(settings)
 	#restart_button.pressed.connect()
 	#exit_button.pressed.connect()
+
+func _input(event):
+	if get_tree().paused == true:
+		if event.is_action_pressed("exit"):
+			unpause()
 
 func unpause():
 	hide()
@@ -23,3 +29,7 @@ func pause():
 	animation_player.play("Pause")
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func settings():
+	hide()
+	canvas_settings.show()
